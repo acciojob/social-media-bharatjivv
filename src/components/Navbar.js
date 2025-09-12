ab2c1240f42fd8cf75cb832b8b1e4328fd7e5f91
BharatJivvTate <hibharatj@gmail.com>
2025-09-12 18:23:32 +0530

    [update] - code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="main-nav">
       <Link to="/">Posts</Link> |<Link to="/users">Users</Link> |
       <Link to="/notifications">Notifications</Link>
     </nav>
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -27,32 +27,34 @@ export default function PostForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        id="postTitle"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <select
-        id="postAuthor"
-        value={user}
-        onChange={(e) => setUser(e.target.value)}
-      >
-        <option value="">Select Author</option>
-        {users.map((u) => (
-          <option key={u} value={u}>
-            {u}
-          </option>
-        ))}
-      </select>
-      <textarea
-        id="postContent"
-        placeholder="Content"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <button type="submit">Add Post</button>
-    </form>
+    <div className="post-form-container">
+      <form onSubmit={handleSubmit} className="post-form">
+        <input
+          id="postTitle"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <select
+          id="postAuthor"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        >
+          <option value="">Select Author</option>
+          {users.map((u) => (
+            <option key={u} value={u}>
+              {u}
+            </option>
+          ))}
+        </select>
+        <textarea
+          id="postContent"
+          placeholder="Content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button type="submit">Add Post</button>
+      </form>
+    </div>
   );
 }
