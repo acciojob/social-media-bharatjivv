215f4740bfa30ce587c53b1a34d861720d2c87fc
BharatJivvTate <hibharatj@gmail.com>
2025-09-17 10:53:14 +0530

    [fix] - main heading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import PostsList from "../components/PostsList";
 export default function Home() {
   return (
     <div>
-      <h1 className="main-heading">Social Media App</h1>
+      <h1 className="main-heading">GenZ</h1>
       <PostForm />
       <PostsList />
     </div>
